cecca8557f2f63feae9b6b6232c13e41193228f7
Nikolay Samygin <nick-ser@yandex.ru>
2019-10-20 18:53:03 +0300

    Remove user's refresh url

--- Smartling.API/Authentication/AuthApiUser.cs
+++ Smartling.API/Authentication/AuthApiUser.cs
@@ -3,7 +3,6 @@
     public class AuthApiUser : AuthApiClient
     {
         protected override string AuthApiV2Authenticate { get { return "/auth-api/v2/authenticate/user"; } }
-        protected override string AuthApiV2Refresh { get { return "/auth-api/v2/authenticate/refresh/user"; } }
 
         public AuthApiUser(string userIdentifier, string userSecret) : base(userIdentifier, userSecret)
         {
